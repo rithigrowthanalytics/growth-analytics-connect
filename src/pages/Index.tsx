191875461fc9e8bef387a8a13d20fa48a7d29e4d
rithigrowthanalytics <rithi@growthanalytics.com.au>
2025-06-24 15:26:44 +1000

    Update Index.tsx
    Tried removing location

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -265,13 +265,6 @@ const Index = () => {
               <h3 className="text-2xl font-semibold text-gray-900 mb-6">Get in Touch</h3>
               <div className="space-y-6">
                 <div className="flex items-start space-x-4">
-                  <MapPin className="h-6 w-6 text-blue-600 mt-1" />
-                  <div>
-                    <h4 className="font-semibold text-gray-900">Sydney Office</h4>
-                    <p className="text-gray-600">Level 42, Governor Phillip Tower<br />1 Farrer Place, Sydney NSW 2000</p>
-                  </div>
-                </div>
-                <div className="flex items-start space-x-4">
                   <Phone className="h-6 w-6 text-blue-600 mt-1" />
                   <div>
                     <h4 className="font-semibold text-gray-900">Phone</h4>
